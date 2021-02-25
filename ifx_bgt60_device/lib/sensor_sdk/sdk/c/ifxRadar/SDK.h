76e3cd7805aae49fe0b33c343f8d78ba57665de7
Yang-IFX <Yang.Wu@infineon.com>
2021-02-25 09:41:48 +0100

    update license

--- ifx_bgt60_device/lib/sensor_sdk/sdk/c/ifxRadar/SDK.h
+++ ifx_bgt60_device/lib/sensor_sdk/sdk/c/ifxRadar/SDK.h
@@ -2,7 +2,7 @@
 * \copyright
 * MIT License
 *
-* Copyright (c) 2020 Infineon Technologies AG
+* Copyright (c) 2021 Infineon Technologies AG
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@
 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE
+* SOFTWARE.
 *
 * \endcopyright
 *
